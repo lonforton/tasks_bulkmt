4bb8ad9eddfbb8590755dfbbf6f85d14362cbb2d
eugene <lonforton@gmail.com>
2019-09-16 15:45:13 +0300

    formatting time to string only if needed

--- commands_queue.h
+++ commands_queue.h
@@ -9,7 +9,7 @@ class CommandsQueue
 {
   public:
 
-    CommandsQueue(std::vector<std::string> command, std::string first_command_time, unsigned int unique_file_id) :
+    CommandsQueue(std::vector<std::string> command, std::chrono::time_point<std::chrono::system_clock> first_command_time, unsigned int unique_file_id) :
     _command(command), _first_command_time(first_command_time), _unique_file_id(unique_file_id) 
     {
 
@@ -20,17 +20,17 @@ class CommandsQueue
     }
 
     std::string get_first_command_timestamp() {
-      return _first_command_time;
+      auto duration = _first_command_time.time_since_epoch();
+      return std::to_string(std::chrono::duration_cast<std::chrono::seconds>(duration).count());
     }
 
     unsigned int get_unique_file_id() {
       return _unique_file_id;
     }
 
-
   private:
     std::vector<std::string> _command;
-    std::string _first_command_time;
+    std::chrono::time_point<std::chrono::system_clock> _first_command_time;
     unsigned int _unique_file_id;     
 };
 
